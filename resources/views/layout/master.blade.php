d916ef2790c0492487cb5173326f4dfbc51e9930
Your Name <you@example.com>
2021-05-18 18:29:18 +0430

    use some stuff in the project

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset("assets/css/bootstrap.min.css") }}">
+    <title>MondayLike</title>
+</head>
+<body>
+    
+
+
+
+    <script src="{{ asset("assets/js/jquery-3.6.0.min.js") }}"></script>
+    <script src="{{ asset("assets/js/app.js") }}"></script>
+</body>
+</html>
