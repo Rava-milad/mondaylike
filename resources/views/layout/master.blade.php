1dcda3a5f01455b21c073aed0b1a2c3a7c1651f5
Your Name <you@example.com>
2021-05-19 23:17:41 +0430

    create something for master page and the some thing like laravel UI

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset("assets/css/bootstrap.min.css") }}">
-    <title>MondayLike</title>
+    <link rel="stylesheet" href="{{ asset("assets/css/style.css") }}">
+    <title>@yield("title , MoonDay")</title>
 </head>
 <body>
     
